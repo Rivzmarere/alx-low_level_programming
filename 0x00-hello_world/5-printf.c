66ca3d8d7c06c78274580a0f675459cdcf38db21
Rivaldo Marere <rivaldo1marere2@gmail.com>
2023-08-10 12:15:51 -0700

    first c-programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,7 @@
  * Description: using the main function 
  * this program prints Programming is like building a multilingual puzzle
  * Return: Always 0 (Success)
- */
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
