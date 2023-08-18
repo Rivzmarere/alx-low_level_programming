9eb5acbe0d16235997db1e3eb623172179a51059
Rivaldo Marere <rivaldo1marere2@gmail.com>
2023-08-18 02:28:53 -0700

    task 0-2

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Test function for positive or negative
+ *
+ * Description: Does my stuff explained.
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
